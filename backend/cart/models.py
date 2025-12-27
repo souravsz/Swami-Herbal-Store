39ea3c66c41b05a18061b0f2b8b244bfc4b87a00
sourav <souravvikas10@gmail.com>
2025-12-28 02:59:46 +0530

    initiated product page and deployment setup

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from products.models import Product
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
